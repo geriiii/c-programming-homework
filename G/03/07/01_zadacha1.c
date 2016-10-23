ccad86e5ec92bdaecad158cdc0636311dabb8a87
geriiii <geri.jecheva@gmail.com>
2016-10-23 17:34:47 +0300

    Update 01_zadacha1.c

--- G/03/07/01_zadacha1.c
+++ G/03/07/01_zadacha1.c
@@ -1,25 +1,19 @@
-#include <stdio.h> 
-
-int main() {
-	int n;
-	int count;
-	int sum = 0;
-	float average;
-	
-	printf("Enter how many numbers you are going to use:\n");
-	scanf("%d", &n);
-	if(n < 1){
-		printf("Error! The number must be bigger that 0!\n");
-		return 0;
-	}
-	for(count = 0; count < n; count++){
-		int number; 
-		scanf("%d", &number);
-		sum += number;
-	}
-
-	average = sum / count;
-	printf("Sum: %d\nAverage: %.2f\n", sum, average);
-
-	return 0;
+#include <stdio.h>
+int main(){
+int n;
+int count;
+int sum = 0;
+float average;
+printf("Enter how many numbers you are going to use:\n");
+scanf("%d", &n);
+if(n < 1){
+printf("Error! The number must be bigger that 0!\n");
+return 0;}
+for(count = 0; count < n; count++){
+int number; 
+scanf("%d", &number);
+sum += number;}
+average = sum / count;
+printf("Sum: %d\nAverage: %.2f\n", sum, average);
+return 0;
 }
